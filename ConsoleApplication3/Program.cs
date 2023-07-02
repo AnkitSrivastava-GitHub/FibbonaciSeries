fdc685bc04e8eed0b65dd529dab08ef7e4c37c4d
Ankit_Srivastava <Ankit Kusum Srivasta@DESKTOP-LPKQJDO>
2023-07-02 12:36:07 +0530

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, t2 = 1, t1 = 0, i,t3;
+            Console.WriteLine("enter the numbers to fibonacci series should be printeed:");
+            n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("series :"+t1+"  "+t2);
+            for (i = 1; i <=n; i++)
+            {
+              //  Console.Write(t1)
+                t3= t1+t2;
+                Console.Write(" "+t1);
+                t1= t2;
+                t2 = t3;
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
